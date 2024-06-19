f07e3b60a9b01229e24fc1fd98e7b957faedaa9c
Brandon Wilson <brandon@coil.com>
2024-06-19 13:41:33 -0500

    feat: combine apps and deployments tables
    Add apps_history table.
    Rename commit column to commitHash.

--- migrations/0002_apps.sql
+++ migrations/0002_apps.sql
@@ -3,22 +3,32 @@
 CREATE TABLE IF NOT EXISTS "apps" (
   "id" text NOT NULL,
   "userId" text NOT NULL,
+  "githubOwner" text NOT NULL,
+  "repo" text NOT NULL,
+  "branch" text NOT NULL,
+  "commitHash" text NOT NULL,
+  "directory" text,
   "status" TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('deployed', 'failed', 'pending')),
   "createdAt" DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
   "updatedAt" DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS "deployments" (
+CREATE TABLE IF NOT EXISTS "apps_history" (
   "id" text NOT NULL,
-  "appId" text NOT NULL,
   "githubOwner" text NOT NULL,
   "repo" text NOT NULL,
   "branch" text NOT NULL,
-  "commit" text NOT NULL,
+  "commitHash" text NOT NULL,
   "directory" text,
-  "createdAt" DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
-  "updatedAt" DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
-  PRIMARY KEY ("id"),
-  FOREIGN KEY ("appId") REFERENCES "apps" ("id") ON DELETE CASCADE
+  "status" TEXT NOT NULL DEFAULT 'pending' CHECK (status IN ('deployed', 'failed', 'pending')),
+  "updatedAt" DATETIME,
+  "historyTimestamp" DATETIME DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now'))
 );
+
+CREATE TRIGGER "after_app_update"
+AFTER UPDATE ON "apps"
+BEGIN
+  INSERT INTO "apps_history" ("id", "githubOwner", "repo", "branch", "commitHash", "directory", "status", "updatedAt")
+  VALUES (OLD."id", OLD."githubOwner", OLD."repo", OLD."branch", OLD."commitHash", OLD."directory", OLD."status", OLD."updatedAt");
+END;
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -33,7 +33,7 @@ export const server = {
 
         // fetch the head commit for repo + branch from GitHub
         const branchUrl = `https://api.github.com/repos/${owner}/${repo}/commits/${branch}`
-        const commit = await fetch(branchUrl, {
+        const commitHash = await fetch(branchUrl, {
           headers: {
             Authorization: `token ${session.accessToken}`,
             "User-Agent": import.meta.env.GITHUB_APP_NAME,
@@ -41,23 +41,20 @@ export const server = {
         }).then((res) => res.json())
 
         const appId = nanoid()
-        const deploymentId = nanoid()
-        context.locals.runtime.env.DB.batch([
-          context.locals.runtime.env.DB.prepare(
-            "INSERT INTO apps (id, userId) VALUES (?1, ?2)",
-          ).bind(appId, session.user.email),
-          context.locals.runtime.env.DB.prepare(
-            "INSERT INTO deployments (id, appId, githubOwner, repo, branch, commit, directory) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
-          ).bind(
-            deploymentId,
+        const info = await context.locals.runtime.env.DB.prepare(
+          "INSERT INTO apps (id, userId, githubOwner, repo, branch, commitHash, directory) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)",
+        )
+          .bind(
             appId,
+            session.user.email,
             owner,
             repo,
             branch,
-            commit.sha,
+            commitHash.sha,
             directory || null,
-          ),
-        ])
+          )
+          .run()
+        console.log(info)
         return { success: true }
       } catch (e) {
         console.error(e)
