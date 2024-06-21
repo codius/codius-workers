9d8a39440bb0137422633e86371e2e16aff5dd12
Brandon Wilson <brandon@coil.com>
2024-06-21 10:23:15 -0500

    feat: replace auth-astro with lucia

--- migrations/0001_auth.sql
+++ migrations/0001_auth.sql
@@ -1,36 +1,14 @@
 -- Migration number: 0001 	 2024-06-18T15:39:07.582Z
 
-CREATE TABLE IF NOT EXISTS "accounts" (
-  "id" TEXT NOT NULL,
-  "userId" TEXT NOT NULL,
-  "type" TEXT NOT NULL,
-  "provider" TEXT NOT NULL,
-  "providerAccountId" TEXT NOT NULL,
-  "refresh_token" TEXT DEFAULT NULL,
-  "access_token" TEXT DEFAULT NULL,
-  "expires_at" NUMBER DEFAULT NULL,
-  "token_type" TEXT DEFAULT NULL,
-  "scope" TEXT DEFAULT NULL,
-  "id_token" TEXT DEFAULT NULL,
-  "session_state" TEXT DEFAULT NULL,
-  "oauth_token_secret" TEXT DEFAULT NULL,
-  "oauth_token" TEXT DEFAULT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS user (
+  id TEXT NOT NULL PRIMARY KEY,
+  github_id NUMBER NOT NULL UNIQUE,
+  username TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "sessions" (
-  "id" TEXT NOT NULL,
-  "sessionToken" TEXT NOT NULL,
-  "userId" TEXT NOT NULL,
-  "expires" DATETIME NOT NULL,
-  PRIMARY KEY (sessionToken)
-);
-
-CREATE TABLE IF NOT EXISTS "users" (
-  "id" TEXT NOT NULL DEFAULT '',
-  "name" TEXT DEFAULT NULL,
-  "email" TEXT DEFAULT NULL,
-  "emailVerified" DATETIME DEFAULT NULL,
-  "image" TEXT DEFAULT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS session (
+  id TEXT NOT NULL PRIMARY KEY,
+  expires_at INTEGER NOT NULL,
+  user_id TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user (id)
 );
