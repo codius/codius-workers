c24913621e05a7d16f6dc170423eae5dc92768cb
Brandon Wilson <brandon@coil.com>
2024-06-26 09:53:05 -0500

    chore: restrict webhook event handling to deploy workflow

--- src/pages/webhooks/github/workflow-job.ts
+++ src/pages/webhooks/github/workflow-job.ts
@@ -1,6 +1,8 @@
 import { Webhooks } from "@octokit/webhooks"
 import type { APIContext } from "astro"
 
+const WORKFLOW_NAME = ".github/workflows/deploy-worker.yml"
+
 export async function POST(context: APIContext): Promise<Response> {
   const webhooks = new Webhooks({
     secret: context.locals.runtime.env.GITHUB_WEBHOOK_SECRET,
@@ -9,7 +11,10 @@ export async function POST(context: APIContext): Promise<Response> {
   webhooks.on("workflow_job.in_progress", async (data) => {
     // steps[1] isn't consistently included in workflow_job.in_progress
     // so we can't reliable get appId + jobId + runId until workflow_job.completed
-    if (data.payload.workflow_job.steps[1]) {
+    if (
+      data.payload.workflow_job.workflow_name === WORKFLOW_NAME &&
+      data.payload.workflow_job.steps[1]
+    ) {
       const appId = data.payload.workflow_job.steps[1].name
       const jobId = String(data.payload.workflow_job.id)
       const runId = String(data.payload.workflow_job.run_id)
@@ -24,25 +29,29 @@ export async function POST(context: APIContext): Promise<Response> {
   })
 
   webhooks.on("workflow_job.completed", async (data) => {
-    const appId = data.payload.workflow_job.steps[1].name
-    const jobId = String(data.payload.workflow_job.id)
-    const runId = String(data.payload.workflow_job.run_id)
-    const status =
-      data.payload.workflow_job.conclusion === "success" ? "deployed" : "failed"
+    if (data.payload.workflow_job.workflow_name === WORKFLOW_NAME) {
+      const appId = data.payload.workflow_job.steps[1].name
+      const jobId = String(data.payload.workflow_job.id)
+      const runId = String(data.payload.workflow_job.run_id)
+      const status =
+        data.payload.workflow_job.conclusion === "success"
+          ? "deployed"
+          : "failed"
 
-    const info = await context.locals.runtime.env.DB.prepare(
-      "UPDATE apps SET githubWorkflowJobId = ?1, githubWorkflowRunId = ?2, status = ?3 WHERE id = ?4",
-    )
-      .bind(jobId, runId, status, appId)
-      .run()
-    console.log(info)
+      const info = await context.locals.runtime.env.DB.prepare(
+        "UPDATE apps SET githubWorkflowJobId = ?1, githubWorkflowRunId = ?2, status = ?3 WHERE id = ?4",
+      )
+        .bind(jobId, runId, status, appId)
+        .run()
+      console.log(info)
+    }
   })
 
   try {
     const body = await context.request.text()
     const eventName = context.request.headers.get("x-github-event")
     const signature = context.request.headers.get("x-hub-signature-256")
-    console.log({ body, eventName, signature })
+
     await webhooks.verifyAndReceive({
       id: context.request.headers.get("x-github-delivery"),
       name: eventName,
