7a88aa0b1d5158d9885b03ae4fd94c801773921b
Brandon Wilson <brandon@coil.com>
2024-07-22 08:10:24 -0500

    feat(dispatch): send request to user worker by subdomain

--- packages/dispatch-worker/src/index.ts
+++ packages/dispatch-worker/src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
   async fetch(request, env, ctx): Promise<Response> {
-    return new Response("Hello World!")
+    try {
+      const workerName = new URL(request.url).host.split(".")[0]
+      const userWorker = env.DISPATCH_NAMESPACE.get(workerName)
+      return await userWorker.fetch(request)
+    } catch (e) {
+      if (e instanceof Error) {
+        if (e.message.startsWith("Worker not found")) {
+          // we tried to get a worker that doesn't exist in our dispatch namespace
+          return new Response("", { status: 404 })
+        }
+
+        // this could be any other exception from `fetch()` *or* an exception
+        // thrown by the called worker (e.g. if the dispatched worker has
+        // `throw MyException()`, you could check for that here).
+        return new Response(e.message, { status: 500 })
+      } else {
+        return new Response("Unexpected error", { status: 500 })
+      }
+    }
   },
 } satisfies ExportedHandler<Env>
