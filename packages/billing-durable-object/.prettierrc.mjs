4dcae39b5ada06051fb221ef3c3541beb25adc99
Brandon Wilson <brandon@coil.com>
2024-07-26 11:29:10 -0500

    feat(billing): add billing durable object

---
+++ packages/billing-durable-object/.prettierrc.mjs
@@ -0,0 +1,5 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  semi: false,
+}
---
+++ packages/billing-durable-object/src/index.ts
@@ -0,0 +1,100 @@
+import { DurableObject } from "cloudflare:workers"
+
+/**
+ * Welcome to Cloudflare Workers! This is your first Durable Objects application.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
+ * - Run `npm run deploy` to publish your application
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/durable-objects
+ */
+
+/**
+ * Associate bindings declared in wrangler.toml with the TypeScript type system
+ */
+export interface Env {
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  MY_DURABLE_OBJECT: DurableObjectNamespace<BillingDurableObject>
+
+  REQUEST_UNIT_PRICE_CENTS: string
+  TOTAL_REQUESTS_PER_UNIT: string
+}
+
+export type WorkerBilling = {
+  totalAllowedWorkerRequests: bigint
+  totalWorkerRequests: bigint
+  totalFundingCents: bigint
+}
+
+/** A Durable Object's behavior is defined in an exported Javascript class */
+export class BillingDurableObject extends DurableObject {
+  /**
+   * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
+   * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
+   *
+   * @param ctx - The interface for interacting with Durable Object state
+   * @param env - The interface to reference bindings declared in wrangler.toml
+   */
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env)
+  }
+
+  /**
+   * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
+   *  Object instance receives a request from a Worker via the same method invocation on the stub
+   *
+   * @returns The greeting to be sent back to the Worker
+   */
+  async sayHello(): Promise<string> {
+    return `Hello, ${this.ctx.id}!`
+  }
+
+  async incrementWorkerRequests(): Promise<WorkerBilling> {
+    let totalWorkerRequests: bigint =
+      (await this.ctx.storage.get("totalWorkerRequests")) || 0n
+    totalWorkerRequests++
+    // You do not have to worry about a concurrent request having modified the value in storage.
+    // "input gates" will automatically protect against unwanted concurrency.
+    // Read-modify-write is safe.
+    await this.ctx.storage.put("totalWorkerRequests", totalWorkerRequests)
+    return {
+      totalWorkerRequests,
+      totalAllowedWorkerRequests:
+        (await this.ctx.storage.get("totalAllowedWorkerRequests")) || 0n,
+      totalFundingCents:
+        (await this.ctx.storage.get("totalFundingCents")) || 0n,
+    }
+  }
+
+  async addWorkerFunds(amountCents: bigint): Promise<WorkerBilling> {
+    let totalFundingCents: bigint =
+      (await this.ctx.storage.get("totalFundingCents")) || 0n
+    totalFundingCents += amountCents
+    await this.ctx.storage.put("totalFundingCents", totalFundingCents)
+
+    const unitPriceCents = BigInt(this.env.REQUEST_UNIT_PRICE_CENTS)
+    const requestsPerUnit = BigInt(this.env.TOTAL_REQUESTS_PER_UNIT)
+
+    // Calculate the total allowed requests
+    const totalAllowedWorkerRequests =
+      (totalFundingCents * requestsPerUnit) / unitPriceCents
+    await this.ctx.storage.put(
+      "totalAllowedWorkerRequests",
+      totalAllowedWorkerRequests,
+    )
+
+    return {
+      totalFundingCents,
+      totalAllowedWorkerRequests,
+      totalWorkerRequests:
+        (await this.ctx.storage.get("totalWorkerRequests")) || 0n,
+    }
+  }
+
+  // async deductMonthlyCost(): Promise<WorkerBilling> {
+  // }
+}
